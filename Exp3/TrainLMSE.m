4a1192a32e9c68d82f3c908353c96304d386a926
Sunguanxiong <747166863@qq.com>
2016-10-13 13:35:14 +0800

    完成多类LMSE的编写和使用给定的TestSamples结果测试

---
+++ Exp3/TrainLMSE.m
@@ -0,0 +1,14 @@
+%通过data和Labels训练感知器，种类数为k
+function [A] = TrainLMSE(Data,Labels, K)
+%DATA,LABELS,k是种类数%
+    [~,n] = size(Data);
+    A = zeros(n,K);
+    %使用一对多的方式训练多类分类器%
+    for i=1:K
+        %将数据分成两类，第i类和非第i类%
+        i
+        Labels_new = 2*ones(size(Labels));
+        Labels_new(Labels == i-1) = 1;
+        A(:,i) = TrainBinaryLMSE(Data,Labels_new);
+    end
+end
